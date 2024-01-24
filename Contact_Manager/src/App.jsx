931a894330985b5895639418ded8335c17763c70
ajjucode <dawreasjad72@gmail.com>
2024-01-24 08:58:12 +0530

    Added Delete Functionality

--- Contact_Manager/src/App.jsx
+++ Contact_Manager/src/App.jsx
@@ -1,26 +1,44 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid"; // Corrected import statement
 import Header from "./component/Header";
 import AddContact from "./component/AddContact";
 import ContactList from "./component/ContactList";
+
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
-  const addContacthandler = (newcontacts) => {
-    console.log(newcontacts);
-    setContacts([...contacts, newcontacts]);
+
+  useEffect(() => {
+    const retrieveContacts =
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+    setContacts(retrieveContacts);
+  }, []);
+
+  const addContacthandler = (newContact) => {
+    setContacts((prevContacts) => {
+      const updatedContacts = [
+        ...prevContacts,
+        { id: uuidv4(), ...newContact },
+      ];
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedContacts));
+      return updatedContacts;
+    });
   };
 
-  useEffect =
-    (() => {
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-    },
-    [contacts]);
+  const removecontacthandler = (id) => {
+    const newcontacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(newcontacts);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newcontacts));
+  };
   return (
     <>
       <Header />
       <AddContact addContacthandler={addContacthandler} />
-      <ContactList contact={contacts} />
+      <ContactList
+        contact={contacts}
+        removeContactHandler={removecontacthandler}
+      />
     </>
   );
 }
--- Contact_Manager/src/component/ContactList.jsx
+++ Contact_Manager/src/component/ContactList.jsx
@@ -1,6 +1,10 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import st from "../Css/ContactList.module.css";
 import user from "../images/user.png";
-const ContactList = ({ contact }) => {
+
+const ContactList = ({ contact, removeContactHandler }) => {
   console.log(contact);
 
   return (
@@ -12,14 +16,23 @@ const ContactList = ({ contact }) => {
         {contact.map((itm, ind) => (
           <div key={ind}>
             <ul className="list-group">
-              <li className="list-group-item ">
-                <img src={user} alt={`Profile of ${itm.name}`} />
-                Name: {itm.name}
-                <br />
-                <small>
-                  Email:
-                  <strong> {itm.email}. </strong>
-                </small>{" "}
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                <div>
+                  <img src={user} alt={`Profile of ${itm.name}`} />
+                  <span>
+                    Name: {itm.name} <br />
+                    <small>
+                      Email:
+                      <strong> {itm.email}. </strong>
+                    </small>
+                  </span>
+                </div>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    onClick={() => removeContactHandler(itm.id)}
+                  />
+                </div>
               </li>
             </ul>
           </div>
@@ -28,4 +41,5 @@ const ContactList = ({ contact }) => {
     </>
   );
 };
+
 export default ContactList;
